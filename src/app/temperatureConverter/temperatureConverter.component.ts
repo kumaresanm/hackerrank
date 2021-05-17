ab7958d2d2d589ec94e2b505c5ef25dbbdfde33f
Kumaresan Muthaliar <k.muthaliar@wolterskluwer.com>
2021-05-17 16:33:12 +0530

    convert cel to far and vice-versa

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,16 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'temperature-converter',
-  templateUrl: './temperatureConverter.component.html',
-  styleUrls: ['./temperatureConverter.component.scss']
+  selector: "temperature-converter",
+  templateUrl: "./temperatureConverter.component.html",
+  styleUrls: ["./temperatureConverter.component.scss"],
 })
+export class TemperatureConverter {
+  farentVal: number;
+  celVal: number;
 
-export class TemperatureConverter implements OnInit {
-
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+  convertCel($event) {
+    this.farentVal = $event * 9 / 5 + 32;
   }
 
+  convertFar($event) {
+    this.celVal = ($event - 32) * 5 / 9;
+  }
 }
